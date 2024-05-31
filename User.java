d6f5128fa2dcb06c307d8e9efd188e5769d32c27
Nick <121282748+NiickSmith@users.noreply.github.com>
2024-05-30 19:18:20 -0500

    User.java
    - Creating boiler plate code for Users in smart home database
    TO DO:
    get user from db
    get/set email
    validate email
    email list and privileges

---
+++ User.java
@@ -0,0 +1,46 @@
+package com.domain.smarthomesystem.data;
+
+import java.util.Objects;
+
+public class User {
+	private String username;
+	private String password;
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return Objects.equals(username, user.username);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(username);
+	}
+	
+	public String toString() {
+		return String.format("\nUsername: %s\nPassword: %s", this.username, this.password);
+	}
+}
